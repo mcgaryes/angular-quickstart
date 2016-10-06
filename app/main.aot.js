b7a49209949558c93dacaf5547a9f6e1ce7d6593
Eric McGary <emcgary@resourceammirati.com>
2016-10-06 13:01:25 -0400

    enabling prod mode

--- app/main.aot.js
+++ app/main.aot.js
@@ -1,5 +1,7 @@
 "use strict";
 var platform_browser_1 = require('@angular/platform-browser');
 var app_module_ngfactory_1 = require('../aot/app/app.module.ngfactory');
+var core_1 = require("@angular/core");
+core_1.enableProdMode();
 platform_browser_1.platformBrowser().bootstrapModuleFactory(app_module_ngfactory_1.AppModuleNgFactory);
 //# sourceMappingURL=main.aot.js.map
